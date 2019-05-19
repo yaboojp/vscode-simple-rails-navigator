c68441ea0abbdd481833b6e45f45d3e51faf8077
yaboojp <yaboo.ja@gmail.com>
2019-05-19 14:36:25 +0900

    simple rails navigator is commitg!!

--- src/extension.ts
+++ src/extension.ts
@@ -6,21 +6,72 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "simple-rails-navigator" is now active!');
+	console.log('Congratulations, your extension "simple-rails-navigator" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	const exclude_path = '**/{tmp,node_modules,vendor}/*';
+	const commandsList = ['Model', 'View', 'Controller', 'Helper', 'Task', 'Spec', 'Javascript', 'Stylesheet'];
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	let simple_rails_navigator = vscode.commands.registerCommand('extension.simple_rails_navigator', () => {
+		vscode.window.showQuickPick(commandsList).then((selectedItem) => {
+            if (selectedItem) {
+				var files;
+				var include_path;
+				switch (selectedItem) {
+					case 'Model':
+						include_path = '**/app/models/**/*.rb';
+						break;
+					case 'View':
+						include_path = '**/app/views/**/*.{erb,haml,slim}';
+						break;
+					case 'Controller':
+						include_path = '**/app/controllers/**/*.rb';
+						break;
+					case 'Helper':
+						include_path = '**/app/helpers/**/*.rb';
+						break;
+					case 'Task':
+						include_path = '**/lib/tasks/**/*.{rb,rake}';
+						break;
+					case 'Spec':
+						include_path = '**/spec/**/*.{rb}';
+						break;
+					case 'Javascript':
+						include_path = '**/app/**/*.{js,coffee}';
+						break;
+					case 'Stylesheet':
+						include_path = '**/app/**/*.{css,scss,sass}';
+						break;
+					default:
+						break;
+				}
+				if (include_path) {
+					files = vscode.workspace.findFiles(include_path, exclude_path);
+				}
+				if (files) {
+					vscode.window.showQuickPick(files.then((urls) => {
+						return urls.map((url) => {
+							var splited_paths = url.path.split('/');
+							var workspace_path = url.path.replace(vscode.workspace.rootPath + "/", "");
+							return {
+										label: workspace_path,
+										// description: splited_paths[splited_paths.length -1],
+										path: url.path.toString()
+									};
+						});
+					})).then((item) => {
+						if (item) {
+							var path = vscode.Uri.parse(item.path);
+							vscode.workspace.openTextDocument(path).then(doc => {
+								vscode.window.showTextDocument(doc);
+							})
+						}
+					});
+				}
+			}
+		});
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(simple_rails_navigator);
 }
 
 // this method is called when your extension is deactivated
